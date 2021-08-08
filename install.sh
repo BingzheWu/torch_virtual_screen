e4770199ea8ba3ee83e4d19304618180184ea572
wubingzhe <wubingzhe@pku.edu.cn>
2021-08-08 18:10:47 +0800

    add install scripts

---
+++ install.sh
@@ -0,0 +1,10 @@
+pip install --upgrade pip
+conda update -n base conda
+conda config --add channels pytorch
+conda config --add channels rdkit
+conda config --add channels conda-forge
+conda config --add channels rmg
+conda install rdkit=2019.03.4.0=py36hc20afe1_1
+conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
+conda install -c dglteam dgllife dgl
+
