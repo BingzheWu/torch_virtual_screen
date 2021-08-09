43758a93e23abb95b515b94ee6d14690b6eb8d94
wubingzhe <wubingzhe@pku.edu.cn>
2021-08-09 19:29:31 +0800

    modify loss function

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ conda config --add channels conda-forge
 conda config --add channels rmg
 conda install rdkit=2019.03.4.0=py36hc20afe1_1
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
-conda install -c dglteam dgllife dgl
+conda install -c dglteam dgllife dgl-cu10.0
+
 
