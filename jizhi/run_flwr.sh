4c1dc5a79adf5635de3157d1e55a8d9104313980
wubingzhe <wubingzhe@pku.edu.cn>
2021-08-17 14:47:59 +0800

    add  flwr test

---
+++ jizhi/run_flwr.sh
@@ -0,0 +1,3 @@
+export PYTHONPATH="${PYTHONPATH}:/apdcephfs/private_bingzhewu/torch_virtual_screen/"
+/softwares/miniconda3/bin/python /apdcephfs/private_bingzhewu/flower/examples/simulation_pytorch/simulation.py
+
